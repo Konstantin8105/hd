1fa90e980ec24efd20db9c281346396b064a0f11
Konstantin8105 <konstantin8105@gmail.com>
2018-08-30 19:25:58 +0300

    prepare error tree

---
+++ errors.go
@@ -0,0 +1,32 @@
+package hd
+
+import "github.com/disiqueira/gotree"
+
+type ErrorTree struct {
+	Name string
+	errs []error
+}
+
+func (e *ErrorTree) Add(err error) {
+	e.errs = append(e.errs, err)
+}
+
+func (e ErrorTree) Error() (s string) {
+	return e.getTree().Print()
+}
+
+func (e ErrorTree) getTree() gotree.Tree {
+	name := "+"
+	if e.Name != "" {
+		name = e.Name
+	}
+	t := gotree.New(name)
+	for _, err := range e.errs {
+		if et, ok := err.(ErrorTree); ok {
+			t.AddTree(et.getTree())
+			continue
+		}
+		t.Add(err.Error())
+	}
+	return t
+}
