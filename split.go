1a5234d192184c6b7a1d678c95772cd76dfc59ee
Konstantin8105 <konstantin8105@gmail.com>
2018-09-03 14:11:13 +0300

    prepare split beam

---
+++ split.go
@@ -0,0 +1,12 @@
+package hd
+
+// SplitBeam is split beam on small parts.
+// Rules of splitting:
+//
+// * beamIndex will beam connected to start beam point
+// * all new point add at the end of point list
+// * all new beam add at the end of beam list
+func (m *Model) SplitBeam(beamIndex, amountIntermediantPoints int) (err error) {
+	// TODO
+	return nil
+}
