95ce30af5f0020d8c2ebd3fd0494454969619a41
Konstantin <konstantin8105@gmail.com>
2018-12-20 13:18:02 +0300

    code style minimization (#9)

--- model_test.go
+++ model_test.go
@@ -1,16 +1,12 @@
 package hd
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/Konstantin8105/cs"
@@ -725,125 +721,6 @@ func TestSplitFail(t *testing.T) {
 
 func TestCodeStyle(t *testing.T) {
 	cs.All(t)
-}
-
-func TestTodo(t *testing.T) {
-	// Show all to do`s in comment code
-	source, err := filepath.Glob(fmt.Sprintf("./%s", "*.go"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var amount int
-
-	for i := range source {
-		t.Run(source[i], func(t *testing.T) {
-			file, err := os.Open(source[i])
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer file.Close()
-
-			pos := 0
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				line := scanner.Text()
-				pos++
-				index := strings.Index(line, "//")
-				if index < 0 {
-					continue
-				}
-				if !strings.Contains(strings.ToUpper(line), "TODO") {
-					continue
-				}
-				t.Logf("%13s:%-4d %s", source[i], pos, line[index:])
-				amount++
-			}
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
-			}
-		})
-	}
-	if amount > 0 {
-		t.Logf("Amount TODO: %d", amount)
-	}
-}
-
-func TestFmt(t *testing.T) {
-	// Show all fmt`s in comments code
-	source, err := filepath.Glob(fmt.Sprintf("./%s", "*.go"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var amount int
-
-	for i := range source {
-		t.Run(source[i], func(t *testing.T) {
-			file, err := os.Open(source[i])
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer file.Close()
-
-			pos := 1
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				line := scanner.Text()
-				pos++
-				index := strings.Index(line, "//")
-				if index < 0 {
-					continue
-				}
-				if !strings.Contains(line, "fmt.") {
-					continue
-				}
-				t.Logf("%d %s", pos, line[index:])
-				amount++
-			}
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
-			}
-		})
-	}
-	if amount > 0 {
-		t.Logf("Amount commented fmt: %d", amount)
-	}
-}
-
-func TestDebug(t *testing.T) {
-	// Show all debug information in code
-	source, err := filepath.Glob(fmt.Sprintf("./%s", "*.go"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for i := range source {
-		t.Run(source[i], func(t *testing.T) {
-			file, err := os.Open(source[i])
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer file.Close()
-
-			pos := 1
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				line := scanner.Text()
-				pos++
-				if !strings.Contains(line, "fmt"+"."+"Print") {
-					continue
-				}
-				t.Errorf("Debug line: %d in file %s", pos, source[i])
-			}
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
-			}
-		})
-	}
 }
 
 func TestWriter(t *testing.T) {
