319fb489ffb4d6dc6b7c032e5ff62a6095ab752e
Konstantin8105 <Konstantin8105@gmail.com>
2021-05-08 16:21:49 +0300

    fix tests

--- theory/axisym.go
+++ theory/axisym.go
@@ -46,24 +46,6 @@ func main() {
 	sm.MaxIteration = -1
 	sm.FloatFormat = 5
 
-
-	constants := ";constant(a1,a2,a3,a4,a5,a6,l);"
-	u := "a1+a2*s"
-	w := "a3+a4*s+a5*s*s+a6*s*s*s"
-	b := cal("b", "d("+w+",s)"+constants+";variable(s);")
-
-	fmt.Println("---------------------------------")
-	fmt.Println("s = 0")
-	cal("u", "inject("+u+",s,0)")
-	cal("w", "inject("+w+",s,0)")
-	cal("b", "inject("+b+",s,0)")
-	fmt.Println("---------------------------------")
-	fmt.Println("s = l")
-	cal("u", "inject("+u+",s,l)")
-	cal("w", "inject("+w+",s,l)")
-	cal("b", "inject("+b+",s,l)")
-	fmt.Println("---------------------------------")
-
 	// -----------------------
 	// CONICAL FINITE ELEMENT STRESS ANALYSIS OF AN AXISYMMETRIC
 	// TANK ON AN ELASTIC FOUNDATION
@@ -89,31 +71,31 @@ func main() {
 		a := "(-e+3*e*e-2*e*e*e)"
 		b := "(6-12*e)"
 		c := "(6*e-6*e*e)"
-		B.Args[B.Position(0,0)] = ast.NewIdent("-cos(fi)/l")
-		B.Args[B.Position(0,1)] = ast.NewIdent("-sin(fi)/l")
-		B.Args[B.Position(0,3)] = ast.NewIdent("cos(fi)/l")
-		B.Args[B.Position(0,4)] = ast.NewIdent("-sin(fi)/l")
+		B.Args[B.Position(0, 0)] = ast.NewIdent("-cos(fi)/l")
+		B.Args[B.Position(0, 1)] = ast.NewIdent("-sin(fi)/l")
+		B.Args[B.Position(0, 3)] = ast.NewIdent("cos(fi)/l")
+		B.Args[B.Position(0, 4)] = ast.NewIdent("-sin(fi)/l")
 
-		B.Args[B.Position(1,0)] = ast.NewIdent("sin(fi)*cos(fi)/r*"+a)
-		B.Args[B.Position(1,1)] = ast.NewIdent("1/r*(1-e-"+a+"*pow(cos(fi),2))")
-		B.Args[B.Position(1,2)] = ast.NewIdent("l*cos(fi)/r*(e-2*e*e+e*e*e)")
-		B.Args[B.Position(1,3)] = ast.NewIdent("sin(fi)*cos(fi)/r*(-"+a+")")
-		B.Args[B.Position(1,4)] = ast.NewIdent("1/r*(e+"+a+"*pow(cos(fi),2))")
-		B.Args[B.Position(1,5)] = ast.NewIdent("l*cos(fi)/r*(-e*e+e*e*e)")
+		B.Args[B.Position(1, 0)] = ast.NewIdent("sin(fi)*cos(fi)/r*" + a)
+		B.Args[B.Position(1, 1)] = ast.NewIdent("1/r*(1-e-" + a + "*pow(cos(fi),2))")
+		B.Args[B.Position(1, 2)] = ast.NewIdent("l*cos(fi)/r*(e-2*e*e+e*e*e)")
+		B.Args[B.Position(1, 3)] = ast.NewIdent("sin(fi)*cos(fi)/r*(-" + a + ")")
+		B.Args[B.Position(1, 4)] = ast.NewIdent("1/r*(e+" + a + "*pow(cos(fi),2))")
+		B.Args[B.Position(1, 5)] = ast.NewIdent("l*cos(fi)/r*(-e*e+e*e*e)")
 
-		B.Args[B.Position(2,0)] = ast.NewIdent("sin(fi)/pow(l,2)*(+("+b+"))")
-		B.Args[B.Position(2,1)] = ast.NewIdent("cos(fi)/pow(l,2)*(-("+b+"))")
-		B.Args[B.Position(2,2)] = ast.NewIdent("1/l*(-4+6*e)")
-		B.Args[B.Position(2,3)] = ast.NewIdent("sin(fi)/pow(l,2)*(-("+b+"))")
-		B.Args[B.Position(2,4)] = ast.NewIdent("cos(fi)/pow(l,2)*(+("+b+"))")
-		B.Args[B.Position(2,5)] = ast.NewIdent("1/l*(-2+6*e)")
+		B.Args[B.Position(2, 0)] = ast.NewIdent("sin(fi)/pow(l,2)*(+(" + b + "))")
+		B.Args[B.Position(2, 1)] = ast.NewIdent("cos(fi)/pow(l,2)*(-(" + b + "))")
+		B.Args[B.Position(2, 2)] = ast.NewIdent("1/l*(-4+6*e)")
+		B.Args[B.Position(2, 3)] = ast.NewIdent("sin(fi)/pow(l,2)*(-(" + b + "))")
+		B.Args[B.Position(2, 4)] = ast.NewIdent("cos(fi)/pow(l,2)*(+(" + b + "))")
+		B.Args[B.Position(2, 5)] = ast.NewIdent("1/l*(-2+6*e)")
 
-		B.Args[B.Position(3,0)] = ast.NewIdent("sin(fi)*sin(fi)/(r*l)*(+("+c+"))")
-		B.Args[B.Position(3,1)] = ast.NewIdent("sin(fi)*cos(fi)/(r*l)*(-("+c+"))")
-		B.Args[B.Position(3,2)] = ast.NewIdent("sin(fi)/r*(1-4*e+3*e*e*e)")
-		B.Args[B.Position(3,3)] = ast.NewIdent("sin(fi)*sin(fi)/(r*l)*(-("+c+"))")
-		B.Args[B.Position(3,4)] = ast.NewIdent("sin(fi)*cos(fi)/(r*l)*(+("+c+"))")
-		B.Args[B.Position(3,5)] = ast.NewIdent("sin(fi)/r*(-2*e+3*e*e*e)")
+		B.Args[B.Position(3, 0)] = ast.NewIdent("sin(fi)*sin(fi)/(r*l)*(+(" + c + "))")
+		B.Args[B.Position(3, 1)] = ast.NewIdent("sin(fi)*cos(fi)/(r*l)*(-(" + c + "))")
+		B.Args[B.Position(3, 2)] = ast.NewIdent("sin(fi)/r*(1-4*e+3*e*e*e)")
+		B.Args[B.Position(3, 3)] = ast.NewIdent("sin(fi)*sin(fi)/(r*l)*(-(" + c + "))")
+		B.Args[B.Position(3, 4)] = ast.NewIdent("sin(fi)*cos(fi)/(r*l)*(+(" + c + "))")
+		B.Args[B.Position(3, 5)] = ast.NewIdent("sin(fi)/r*(-2*e+3*e*e*e)")
 	}
 	fmt.Fprintf(os.Stdout, "B = %s\n", B.String())
 
