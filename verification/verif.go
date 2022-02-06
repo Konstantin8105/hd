f42a99aef639867bfab08416168d43f045d24040
Konstantin8105 <Konstantin8105@gmail.com>
2022-02-06 13:17:16 +0300

    verif: add test

--- verification/verif.go
+++ verification/verif.go
@@ -1,19 +1,34 @@
 package verif
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/Konstantin8105/hd"
+	"github.com/Konstantin8105/pow"
 )
 
-func compareD(actual, expect [3]float64) (tol []float64) {
+func compare(actual, expect float64) string {
+	var tol float64
+	if expect == actual {
+		tol = 0
+	} else if expect != 0.0 {
+		tol = (actual - expect) / expect
+	} else {
+		tol = (actual - expect) / actual
+	}
+	tol *= 100.0
+	return fmt.Sprintf("%9.4e %9.4e : %7.2f%%\n", actual, expect, tol)
+}
+
+func compareD(actual, expect [3]float64) (str string) {
 	for i := range actual {
-		tol = append(tol, (actual[i]-expect[i])/expect[i]*100.0)
+		str += compare(actual[i], expect[i])
 	}
 	return
 }
 
-func MSA21() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)) {
+func MSA21() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
 	name = `
 Book:
 William McGuire, Richard H.Gallagher, Ronald D.Ziemian
@@ -49,7 +64,7 @@ Page 21
 		},
 	}
 
-	isOk = func(lc *hd.LoadCase) (tol []float64) {
+	isOk = func(lc *hd.LoadCase) (str string) {
 		var (
 			expectX  = 2.41 / 1000.0
 			expectY  = 0.72 / 1000.0
@@ -58,18 +73,18 @@ Page 21
 		)
 		x := lc.PointDisplacementGlobal[1][0]
 		y := lc.PointDisplacementGlobal[1][1]
-		tol = append(tol, (x-expectX)/expectX*100.0)
-		tol = append(tol, (y-expectY)/expectY*100.0)
+		str += fmt.Sprintf("def x: %s", compare(x, expectX))
+		str += fmt.Sprintf("def y: %s", compare(y, expectY))
 		Fa := lc.BeamForces[0][3]
 		Fb := lc.BeamForces[1][3]
-		tol = append(tol, (Fa-expectFa)/expectFa*100.0)
-		tol = append(tol, (Fb-expectFb)/expectFb*100.0)
+		str += fmt.Sprintf("fa : %s", compare(Fa, expectFa))
+		str += fmt.Sprintf("fa : %s", compare(Fb, expectFb))
 		return
 	}
 	return
 }
 
-func MSA49() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)) {
+func MSA49() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
 	name = `
 Book:
 William McGuire, Richard H.Gallagher, Ronald D.Ziemian
@@ -106,7 +121,7 @@ Page 80
 		},
 	}
 
-	isOk = func(lc *hd.LoadCase) (tol []float64) {
+	isOk = func(lc *hd.LoadCase) (str string) {
 		var (
 			expectReactXa = -3.6e3
 			expectReactYa = -3.3e3
@@ -115,18 +130,18 @@ Page 80
 			expectDXc     = -19.15e-3
 			expectDRc     = -0.00530
 		)
-		tol = append(tol, (lc.Reactions[0][0]-expectReactXa)/expectReactXa*100.0)
-		tol = append(tol, (lc.Reactions[0][1]-expectReactYa)/expectReactYa*100.0)
-		tol = append(tol, (lc.Reactions[0][2]-expectReactMa)/expectReactMa*100.0)
-		tol = append(tol, (lc.Reactions[1][1]-expectReactYb)/expectReactYb*100.0)
-		tol = append(tol, (lc.PointDisplacementGlobal[2][1]-expectDXc)/expectDXc*100.0)
-		tol = append(tol, (lc.PointDisplacementGlobal[2][2]-expectDRc)/expectDRc*100.0)
+		str += fmt.Sprintf("react: %s", compare(lc.Reactions[0][0], expectReactXa))
+		str += fmt.Sprintf("react: %s", compare(lc.Reactions[0][1], expectReactYa))
+		str += fmt.Sprintf("react: %s", compare(lc.Reactions[0][2], expectReactMa))
+		str += fmt.Sprintf("react: %s", compare(lc.Reactions[1][1], expectReactYb))
+		str += fmt.Sprintf("defor: %s", compare(lc.PointDisplacementGlobal[2][1], expectDXc))
+		str += fmt.Sprintf("defor: %s", compare(lc.PointDisplacementGlobal[2][2], expectDRc))
 		return
 	}
 	return
 }
 
-func MSA413() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)) {
+func MSA413() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
 	name = `
 Book:
 William McGuire, Richard H.Gallagher, Ronald D.Ziemian
@@ -164,7 +179,7 @@ Page 84
 		},
 	}
 
-	isOk = func(lc *hd.LoadCase) (tol []float64) {
+	isOk = func(lc *hd.LoadCase) string {
 		var (
 			expectDa = [3]float64{
 				+0.4414 / 1000.0,
@@ -172,8 +187,60 @@ Page 84
 				+0.00169,
 			}
 		)
-		tol = append(tol, compareD(lc.PointDisplacementGlobal[1], expectDa)...)
-		return
+		return compareD(lc.PointDisplacementGlobal[1], expectDa)
+	}
+	return
+}
+
+func MSA67() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
+	name = `
+Book:
+William McGuire, Richard H.Gallagher, Ronald D.Ziemian
+Matrix Structural Analysis
+
+EXAMPLE 6.7
+Page 168
+`
+	// random values
+	L := 3 * math.Pi
+	J := 50e-6
+	A, E := 1.0, 2e11
+	model = hd.Model{
+		Points: [][2]float64{
+			{0, 0},
+			{L / 3.0, 0},
+			{L/3.0 + L/6.0, 0},
+			{L, 0},
+		},
+		Beams: []hd.BeamProp{
+			{N: [2]int{0, 1}, A: A, J: J, E: E},
+			{N: [2]int{1, 2}, A: A, J: J, E: E},
+			{N: [2]int{2, 3}, A: A, J: J, E: E},
+		},
+		Pins: [][6]bool{
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+		},
+		Supports: [][3]bool{
+			{true, true, true},
+			{false, false, false},
+			{false, false, false},
+			{true, true, true},
+		},
+	}
+	P := 100e3
+	lc = hd.LoadCase{
+		LoadNodes: []hd.LoadNode{
+			{N: 1, Forces: [3]float64{0, -P, 0}},
+		},
+	}
+
+	isOk = func(lc *hd.LoadCase) string {
+		var (
+			v = -(5.0 * P * pow.E3(L)) / (1296.0 * E * J)
+		)
+		return compare(lc.PointDisplacementGlobal[2][1], v)
 	}
 	return
 }
@@ -182,7 +249,7 @@ Page 84
 // TODO: add Problem 6.12
 // TODO: add MSA74, MSA75, MSA76, MSA710
 
-func MSA81() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)) {
+func MSA81() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
 	name = `
 Book:
 William McGuire, Richard H.Gallagher, Ronald D.Ziemian
@@ -196,6 +263,7 @@ Page 220
 	k := 1e3
 	Area := 1e-3
 	E := k * L / Area
+	factor := 1e3
 	model = hd.Model{
 		Points: [][2]float64{
 			{L, 0}, // a
@@ -203,12 +271,24 @@ Page 220
 			{0, L}, // c
 		},
 		Beams: []hd.BeamProp{
-			{N: [2]int{0, 1}, A: Area * 40, J: 1 * 40, E: E * 40}, // ab
-			{N: [2]int{1, 2}, A: Area, J: 1, E: E},                // bc
+			// ab
+			{
+				N: [2]int{0, 1},
+				A: factor * Area,
+				J: factor * 1,
+				E: factor * E,
+			},
+			// bc
+			{N: [2]int{1, 2},
+				A: Area,
+				J: 1,
+				E: E,
+			},
 		},
 		Pins: [][6]bool{
 			{false, true, true, false, true, true},
-			{false, false, false, false, false, false},
+			{false, true, true, false, true, true},
+			// {false, false, false, false, false, false},
 		},
 		Supports: [][3]bool{
 			{true, true, false},
@@ -216,8 +296,14 @@ Page 220
 			{true, false, true},
 		},
 	}
-	P := k * L
 	a := 0.1
+	var (
+		expectQ = math.Atan(math.Pow(a, 1.0/3.0))
+		expectD = L * math.Sin(expectQ)
+		expectP = k * L * math.Cos(expectQ) / (1.0 + a*1.0/math.Tan(expectQ))
+	)
+	P := 0.99 * expectP // k * L
+
 	lc = hd.LoadCase{
 		LoadNodes: []hd.LoadNode{
 			{N: 1, Forces: [3]float64{a * P, -P, 0}},
@@ -229,73 +315,48 @@ Page 220
 	lc.NonlinearNR.Substep = 50
 
 	lc.NonlinearNK.MaxIterations = 29000
-	lc.NonlinearNK.Substep = 50.0
+	lc.NonlinearNK.Substep = 50
 
 	// TODO: add arc-method
 
-	isOk = func(lc *hd.LoadCase) (tol []float64) {
+	isOk = func(lc *hd.LoadCase) (str string) {
 		var (
 			expectLinearBucklingFactor = 1.0
 		)
-		tol = append(tol, (lc.LinearBuckling.Results[0].Factor-expectLinearBucklingFactor)/
-			expectLinearBucklingFactor*100.0)
-
-		var (
-			expectQ = math.Atan(math.Pow(a, 1.0/3.0))
-			expectD = L * math.Sin(expectQ)
-			expectP = k * L * math.Cos(expectQ) / (1.0 + a*1.0/math.Tan(expectQ))
-		)
+		str += fmt.Sprintf("Linear buckl: %s",
+			compare(lc.LinearBuckling.Results[0].Factor, expectLinearBucklingFactor))
+		str += fmt.Sprintf("expect Q : %.4e\n", expectQ)
+		str += fmt.Sprintf("expect D : %.4e\n", expectD)
+		str += fmt.Sprintf("expect P : %.4e  %.4e\n", expectP, expectP/(k*L))
+		// compare with linear result
+		str += fmt.Sprintf("Deformation: %s",
+			compare(lc.PointDisplacementGlobal[1][0], expectD))
+		// 		for Q := 0.0; Q < math.Pi/2.0; Q += 0.01 {
+		// 			expectP = k * L * math.Cos(Q) / (1.0 + a*1.0/math.Tan(Q))
+		// 			fmt.Println(Q*180/3.1415, Q, expectP)
+		// 		}
 		// TODO for each steps
 		// TODO for arc method in point with Yc == 0
-		// for _, res := range lc.NonlinearNK.Results {
-		// 	d := res.PointDisplacementGlobal[1][0]
-		// 	p := res.Reactions[0][1]
-		// 	Q := math.Asin(d / L)
-		// 	expectP := k * L * math.Cos(Q) / (1.0 + a*1.0/math.Tan(Q))
-		// 	tol = append(tol, (p-expectP)/expectP*100.0)
-		// }
-		// for _, res := range lc.NonlinearNR.Results {
-		// 	d := res.PointDisplacementGlobal[1][0]
-		// 	p := res.Reactions[0][1]
-		// 	Q := math.Asin(d / L)
-		// 	expectP := k * L * math.Cos(Q) / (1.0 + a*1.0/math.Tan(Q))
-		// 	tol = append(tol, (p-expectP)/expectP*100.0)
-		// }
-
-		for i := range lc.NonlinearNK.Results {
-			if i == 0 {
-				continue
-			}
-			lastD := lc.NonlinearNK.Results[i-1].PointDisplacementGlobal[1][0]
-			presD := lc.NonlinearNK.Results[i].PointDisplacementGlobal[1][0]
-			if lastD <= expectD && expectD <= presD {
-				lastP := lc.NonlinearNK.Results[i-1].Reactions[0][1]
-				presP := lc.NonlinearNK.Results[i].Reactions[0][1]
-				p := lastP + (presP-lastP)*(expectD-lastD)/(presD-lastD)
-				tol = append(tol, (p-expectP)/expectP*100.0)
-			}
+		for _, res := range lc.NonlinearNK.Results {
+			d := res.PointDisplacementGlobal[1][0]
+			p := res.Reactions[0][1]
+			Q := math.Asin(d / L)
+			expectP := k * L * math.Cos(Q) / (1.0 + a*1.0/math.Tan(Q))
+			str += fmt.Sprintf("Steps NK: %s", compare(p, expectP))
 		}
-
-		for i := range lc.NonlinearNR.Results {
-			if i == 0 {
-				continue
-			}
-			lastD := lc.NonlinearNR.Results[i-1].PointDisplacementGlobal[1][0]
-			presD := lc.NonlinearNR.Results[i].PointDisplacementGlobal[1][0]
-			if lastD <= expectD && expectD <= presD {
-				lastP := lc.NonlinearNR.Results[i-1].Reactions[0][1]
-				presP := lc.NonlinearNR.Results[i].Reactions[0][1]
-				p := lastP + (presP-lastP)*(expectD-lastD)/(presD-lastD)
-				tol = append(tol, (p-expectP)/expectP*100.0)
-			}
+		for _, res := range lc.NonlinearNR.Results {
+			d := res.PointDisplacementGlobal[1][0]
+			p := res.Reactions[0][1]
+			Q := math.Asin(d / L)
+			expectP := k * L * math.Cos(Q) / (1.0 + a*1.0/math.Tan(Q))
+			str += fmt.Sprintf("Steps NR: %s", compare(p, expectP))
 		}
-
 		return
 	}
 	return
 }
 
-func MSA91() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)) {
+func MSA91() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string) {
 	name = `
 Book:
 William McGuire, Richard H.Gallagher, Ronald D.Ziemian
@@ -339,7 +400,7 @@ Page 247
 
 	// TODO: add arc-method
 
-	isOk = func(lc *hd.LoadCase) (tol []float64) {
+	isOk = func(lc *hd.LoadCase) (str string) {
 		var (
 			expectD = 1692.0 / 1000.0
 			expectP = 340.0 * 1000.0
@@ -354,7 +415,7 @@ Page 247
 				lastP := lc.NonlinearNK.Results[i-1].Reactions[2][1]
 				presP := lc.NonlinearNK.Results[i].Reactions[2][1]
 				p := lastP + (presP-lastP)*(expectD-lastD)/(presD-lastD)
-				tol = append(tol, (p-expectP)/expectP*100.0)
+				str += fmt.Sprintf("P NK: %s", compare(p, expectP))
 			}
 		}
 
@@ -368,7 +429,7 @@ Page 247
 				lastP := lc.NonlinearNR.Results[i-1].Reactions[2][1]
 				presP := lc.NonlinearNR.Results[i].Reactions[2][1]
 				p := lastP + (presP-lastP)*(expectD-lastD)/(presD-lastD)
-				tol = append(tol, (p-expectP)/expectP*100.0)
+				str += fmt.Sprintf("P NK: %s", compare(p, expectP))
 			}
 		}
 		return
--- verification/verif_test.go
+++ verification/verif_test.go
@@ -1,60 +1,69 @@
 package verif
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"strings"
+	"testing"
 
 	"github.com/Konstantin8105/hd"
+	"github.com/pmezard/go-difflib/difflib"
 )
 
-func Example() {
-	tcs := []func() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) (tol []float64)){
-		MSA21,
-		MSA49,
-		MSA413,
-		MSA81,
-		MSA91,
+func Test(t *testing.T) {
+	tcs := []struct {
+		name string
+		f    func() (model hd.Model, lc hd.LoadCase, name string, isOk func(lc *hd.LoadCase) string)
+	}{
+		{name: "MSA21", f: MSA21},
+		{name: "MSA49", f: MSA49},
+		{name: "MSA413", f: MSA413},
+		{name: "MSA67", f: MSA67},
+		{name: "MSA81", f: MSA81},
+		{name: "MSA91", f: MSA91},
 	}
 
 	for _, tc := range tcs {
-		model, lc, name, isOk := tc()
-		name = strings.ReplaceAll(name, "\n", " ")
-		name = strings.ReplaceAll(name, "  ", " ")
-		name = strings.TrimSpace(name)
-		fmt.Fprintf(os.Stdout, "%s\n", name)
-		err := hd.LinearStatic(nil, &model, &lc)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "%v\n%v\n", err, lc)
-			continue
-		}
-		tols := isOk(&lc)
-		for i := range tols {
-			fmt.Fprintf(os.Stdout, "case %2d: %7.2f%%\n", i, tols[i])
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			var actual bytes.Buffer
+			{
+				model, lc, name, isOk := tc.f()
+				fmt.Fprintf(&actual, "name : %s\n", name)
+				err := hd.LinearStatic(nil, &model, &lc)
+				if err != nil {
+					t.Fatalf("%v\n%v\n", err, lc)
+				}
+				fmt.Fprintf(&actual, "%s\n", isOk(&lc))
+			}
+
+			// compare files
+			if os.Getenv("UPDATE") != "" {
+				err := ioutil.WriteFile("."+tc.name, actual.Bytes(), 0644)
+				if err != nil {
+					t.Fatalf("Cannot Update: %v", err)
+				}
+			}
+
+			expect, err := ioutil.ReadFile("." + tc.name)
+			if err != nil {
+				t.Fatalf("Cannot read file : %v", err)
+			}
+
+			if !bytes.Equal(expect, actual.Bytes()) {
+				// show a diff between files
+				diff := difflib.UnifiedDiff{
+					A:        difflib.SplitLines(string(expect)),
+					B:        difflib.SplitLines(actual.String()),
+					FromFile: "Original",
+					ToFile:   "Current",
+					Context:  30000,
+				}
+				text, _ := difflib.GetUnifiedDiffString(diff)
+				t.Log(text)
+				t.Errorf("result is not same")
+			}
+		})
+
 	}
-	// Output:
-	// Book: William McGuire, Richard H.Gallagher, Ronald D.Ziemian Matrix Structural Analysis EXAMPLE 2.1 Page 21
-	// case  0:    0.05%
-	// case  1:    0.46%
-	// case  2:    0.00%
-	// case  3:   -0.01%
-	// Book: William McGuire, Richard H.Gallagher, Ronald D.Ziemian Matrix Structural Analysis EXAMPLE 4.9 Page 80
-	// case  0:   -1.79%
-	// case  1:    0.44%
-	// case  2:    0.44%
-	// case  3:    0.00%
-	// case  4:    0.00%
-	// case  5:    0.06%
-	// Book: William McGuire, Richard H.Gallagher, Ronald D.Ziemian Matrix Structural Analysis EXAMPLE 4.13 Page 84
-	// case  0:    0.02%
-	// case  1:    0.02%
-	// case  2:    0.26%
-	// Book: William McGuire, Richard H.Gallagher, Ronald D.Ziemian Matrix Structural Analysis EXAMPLE 8.1 Page 220
-	// case  0:    0.00%
-	// case  1:    8.17%
-	// case  2:    8.17%
-	// Book: William McGuire, Richard H.Gallagher, Ronald D.Ziemian Matrix Structural Analysis EXAMPLE 9.1 Page 247
-	// case  0:    5.05%
-	// case  1:    5.05%
 }
